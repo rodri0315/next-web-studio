8ebe6891e04b65e8b222187e712e74b090b84e80
Jorge Rodriguez <jorge@headway.io>
2023-09-18 21:47:07 -0600

    update footer for business

--- components/footer.js
+++ components/footer.js
@@ -7,11 +7,11 @@ export default function Footer() {
     <footer className="bg-black ph3 pv4 white">
       <div className="mw7 center pt3">
         <div className="measure-narrow center mb4">
-          <img className="db w4 center mb4 br0" src="/img/logo.svg" alt="Kaldi logo" />
+          <img className="db w4 center mb4 br0" src="/img/spp-logo.jpeg" alt="Santos Paving Pros logo" />
         </div>
         <div className="flex-ns justify-between">
           <div>
-            <h3 className="f4 b lh-title mb1 primary">Iron Detailing</h3>
+            <h3 className="f4 b lh-title mb1 primary">Santos Paving Pros</h3>
             <ul className="mb3">
               <li><a href="/" className="link">Home</a></li>
               <li><a href="/services" className="link">Our Services</a></li>
@@ -19,17 +19,18 @@ export default function Footer() {
             </ul>
           </div>
           <div>
-            <h3 className="f4 b lh-title mb1 primary">Serving Torrance and South Bay</h3>
+            <h3 className="f4 b lh-title mb1 primary">Serving The Triangle Area</h3>
             <ul className="mb3">
-              <li><a href="tel:3239447238" className="link">Call or Text to Book<br />(323) 944-7238</a></li>
-              <li><a target="_blank" rel="noreferrer" href="https://squareup.com/appointments/book/afwc68r654y6i1/L8VCJCD01JR06/services" className="link">Book Online</a></li>
+              <li><a href="tel:9194285063" className="link">Call or Text<br />(919) 428-5063</a></li>
+              {/* <li><a target="_blank" rel="noreferrer" href="https://squareup.com/appointments/book/afwc68r654y6i1/L8VCJCD01JR06/services" className="link">Book Online</a></li> */}
             </ul>
           </div>
           <div>
-            <h3 className="f4 b lh-title mb2 primary">Social media</h3>
+            <h3 className="f4 b lh-title mb2 primary">Social Media</h3>
             <ul className="mhn2">
               {/* <SocialIcon link="#" iconPath="/img/icons-facebook.svg" /> */}
-              <SocialIcon link="https://www.instagram.com/irondetails/" iconPath="/img/icons-instagram.svg" />
+              {/* <SocialIcon link="https://www.instagram.com/irondetails/" iconPath="/img/icons-instagram.svg" /> */}
+              <SocialIcon link="https://www.facebook.com/SantosPavingProsLLC" iconPath="/img/icons-facebook.svg" />
             </ul>
           </div>
         </div>
