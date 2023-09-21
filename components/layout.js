36646075fce44abe422ce1ac745e0d3d8b3cd235
Jorge Rodriguez <jorge@headway.io>
2023-09-20 23:21:34 -0400

    add analytics

--- components/layout.js
+++ components/layout.js
@@ -2,14 +2,18 @@ import Nav from '../components/nav'
 import Footer from '../components/footer'
 import Meta from '../components/meta'
 import metaData from '../data/meta.json'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Layout({ children }) {
   return (
     <>
       <Meta />
       <div className="min-h-screen">
-        <Nav logo={metaData.logo}/>
-        <main>{children}</main>
+        <Nav logo={metaData.logo} />
+        <main>
+          {children}
+          <Analytics />
+        </main>
       </div>
       <Footer />
     </>
